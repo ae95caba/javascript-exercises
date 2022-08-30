036292220e924904a7743488dadf2826715de344
Andre Espinoza <ae95caba@gmail.com>
2022-08-29 23:28:14 -0300

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
-};
+const number = Math.floor(Math.random() * 1000);
+const repeatString = function(string,num) {
+  var finalString ="";
+  var i=0;
+  
+  if(num==0 || num>0){
+  while  (i<num){
+    finalString+=string;
+    ++i;}
+ 
+  return finalString;}
+  else if (num<0){
+    return "ERROR";
+  }
+  };
+  
+  
+  console.log(repeatString("hey",-1));
 
 // Do not edit below this line
 module.exports = repeatString;
