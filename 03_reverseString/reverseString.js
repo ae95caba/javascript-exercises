059b99e1fdfa83e0f6a71a4e68b34e37b143f982
Andre Espinoza <ae95caba@gmail.com>
2022-08-30 09:49:10 -0300

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+
+const reverseString = function(string) {
+    var reversedString="";
+    
+    
+
+    
+    
+    for (var i=1 ; i<=string.length ; ++i){
+        
+        var currentLetter=string.charAt(string.length-i);
+        reversedString+= currentLetter;
+        
+    }
+    
+    return reversedString;
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
