7c9e471b5ebae392d2c6ff086cf172d585dc0a1e
Andre Espinoza <ae95caba@gmail.com>
2022-08-31 01:58:13 -0300

    get test 5 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,24 @@
 const removeFromArray = function(array,position) {
-    for (i=1;i<arguments.length;i++ ) {      
-        if (arguments[i]>=0 && arguments[i]<=array.length){
+    //turns arguemtens array into a real array
+    
+    const args = Array.prototype.slice.call(arguments);
+    
+    //creates an array with only the positions parameters, that way i can sort them in descening order
+    //so i can start removing form the higher position that can pass the if "test", that way
+    // i can remove every position starting from the starter parameter array, no matter the order of removal
+
+    var arrayPositions = args.slice(1);
+    arrayPositions.sort();
+    arrayPositions.reverse();
+    
+    //this for makes i turn into every position parameter
+    for (i=0;i<arrayPositions.length;i++ ) {    
+        //if the current parameter (i) is a position that exists do  
+        if (arrayPositions[i]>=0 && arrayPositions[i]<=array.length){
+
             
-            array.splice((arguments[i]-1),1);
+            //remove that position from the array parameter
+            array.splice((arrayPositions[i]-1),1);
             
             }
         }   
@@ -10,6 +26,5 @@ const removeFromArray = function(array,position) {
     return array;
     
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
