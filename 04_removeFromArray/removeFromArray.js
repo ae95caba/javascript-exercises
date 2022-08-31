339eaa986ca91738bf2d069d144d46b32538ddd0
Andre Espinoza <ae95caba@gmail.com>
2022-08-30 23:04:30 -0300

    resolve thrid test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,position1,position2) {
+            
+    if (position1>=0 && position1<=array.length){
+        array.splice((position1-1),1);
+    }
+    if (position2>=0 && position2<=array.length){
+        array.splice((position2-1),1);
+    }
+        return array;
 };
 
 // Do not edit below this line
