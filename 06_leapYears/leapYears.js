b8be61e1808275e85ddc7e37ea1e0f9599cc0ba2
Andre Espinoza <ae95caba@gmail.com>
2022-09-02 00:45:42 -0300

    get all the test done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+          
+    if (year%100===0 ){
+        if (year%400===0){
+            return true;
+        }else{ 
+            return false;
+        }
+    }
 
-};
-
+    return year%4===0;
+}
 // Do not edit below this line
 module.exports = leapYears;
