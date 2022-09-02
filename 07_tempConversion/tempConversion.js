2f93afe8e3aa18c1c3ed4d3f4ed6fd4feb79ddb8
Andre Espinoza <ae95caba@gmail.com>
2022-09-02 01:37:33 -0300

    get all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  exact = (f - 32) * (5/9);
+  let rounded = Math.round(exact*10)/10;
+  return rounded
+  
 };
 
-const ctof = function() {
-
-};
-
+const ctof = function(c) {
+  let exact = c * 1.8 + 32;
+  let rounded = Math.round(exact*10)/10;
+  return rounded
+}
 // Do not edit below this line
 module.exports = {
   ftoc,
