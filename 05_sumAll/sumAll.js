a7348a1c112d64231c4f4f1624e92f4b0792d039
Andre Espinoza <ae95caba@gmail.com>
2022-09-01 23:24:57 -0300

    get all test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(par1, par2) {
+    if(par1<0 || par2<0||typeof par1 != "number"||typeof par2 != "number"){
+        return "ERROR";
+    }
+    var sum = 0;
+    if(par1<par2){
+        for (let i = par1; i<=par2;i++){
+        sum+=i;
+        }
+        return sum;
+    }
 
-};
-
+    if(par2<par1){
+        for(let i=par2; i<=par1; i++){
+        sum+=i;
+        }
+        return sum;
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
