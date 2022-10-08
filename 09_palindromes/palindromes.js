ec46f212baa80d2ddc5aeaaf8c0678a150c9c6f1
Andre Espinoza <ae95caba@gmail.com>
2022-10-08 05:28:21 -0300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    allLower= string.toLowerCase();
+    newString=[]
+    reversedString=[]
+    for(let i=0; i<allLower.length; i++){
+        if( allLower.charAt(i) !== "!" && allLower.charAt(i) !== " " && allLower.charAt(i) !== "," && allLower.charAt(i) !== ".")
+       newString.push(allLower.charAt(i)) ;
+    }
+    for(let i=allLower.length; i>0; i--){
+        if( allLower.charAt(i-1) !== "!" && allLower.charAt(i-1) !== " " && allLower.charAt(i-1) !== "," && allLower.charAt(i-1) !== ".")
+       reversedString.push(allLower.charAt(i-1)) ;
+    }
+    let checkEquality=  (newString.length == reversedString.length) && newString.every(function(element, index) {
+        return element === reversedString[index];
+    });
+    return checkEquality;
+    
 
 };
 
