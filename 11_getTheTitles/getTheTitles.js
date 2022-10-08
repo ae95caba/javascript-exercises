7e3c7508d4e1e95f7e75c054d5ab02a6908f7fe4
Andre Espinoza <ae95caba@gmail.com>
2022-10-08 07:20:53 -0300

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfObjets) {
+   
+    var titles = arrayOfObjets.map(object => object.title);
+    //return arrayOfObjets[0].title;
+    return titles;
 };
 
 // Do not edit below this line
